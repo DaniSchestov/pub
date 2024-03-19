97796828a3e13fccb8596da78e92f6be4c2763f3
DaniSchestov <101457281+DaniSchestov@users.noreply.github.com>
2024-03-19 18:53:34 +0300

    Create fi2gi.sh

---
+++ fi2gi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+giversion="1.7.1"
+#Check if FI Agent is installed
+if [ -f /etc/fusioninventory/agent.cfg ]
+then
+    fitag=$(awk '/^[Tt]ag = /{print $3}' /etc/fusioninventory/agent.cfg)
+    echo $fitag
+    apt purge fusioninventory* -y
+    else
+    echo "No FI Agent installed"
+fir
+if [ ! -f /etc/glpi-agent/agent.cfg ] && [ ! -f /etc/glpi-agent/conf.d/00-install.cfg ]
+then
+    apt install -y wget
+    wget "https://github.com/glpi-project/glpi-agent/releases/download/$giversion/glpi-agent-$giversion-linux-installer.pl" -O /tmp/glpi-agent-$giversion-linux-installer.pl
+    perl /tmp/glpi-agent-$giversion-linux-installer.pl --server="https://glpi.osrc.it/plugins/fusioninventory/,https://glpi.osrc.it/plugins/glpiinventory/" --tag=$fitag --service --install --runnow
+    rm /tmp/glpi-agent-$giversion-linux-installer.pl
+else
+    echo GLPI Agent is already installed
+fi
